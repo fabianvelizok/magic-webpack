bd9aca5997aebda7194be904b716bab17fd689f5
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-03-18 00:12:34 -0300

    Add the logo inside a new component (header)

--- react/src/js/index.js
+++ react/src/js/index.js
@@ -1,19 +1,17 @@
 import '../css/index.css';
-
-import platziImage from '../images/platzi.png';
-
-/* import render from './render'; */
-import makeImage from './makeImage';
 import data from './teachers.json';
 
 // React
 import React from 'react';
 import { render } from 'react-dom';
 
+// Components
 import Teachers from './components/teachers';
+import Header from './components/header';
 
-const container = document.getElementById('teachers');
-render(<Teachers teachers={data.teachers}/>, container);
+const headerContainer = document.getElementById('header');
+render(<Header />, headerContainer);
 
-/* const img = makeImage(platziImage);
-render(img); */
+const teacherContainer = document.getElementById('teachers');
+render(<Teachers teachers={data.teachers}/>, teacherContainer);
+
