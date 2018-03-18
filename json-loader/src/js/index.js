c10a5e3cb303fa48de338b1bf013555867b9e5dd
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-03-17 21:55:34 -0300

    Print a list of teachers got from a json file

---
+++ json-loader/src/js/index.js
@@ -0,0 +1,26 @@
+import '../css/index.css';
+
+import platziImage from '../images/platzi.png';
+
+import message from './message';
+import render from './render';
+import makeImage from './makeImage';
+import data from './teachers.json';
+
+data.teachers.forEach( (t) => {
+  const teacher = document.createElement('li');
+  teacher.innerHTML = `
+    <a href="http://twitter.com/${t.twitter}"
+       target="_blank"
+    >${t.name}</a>
+  `;
+  render('#teachers', teacher);
+});
+
+console.log('Babel...');
+console.log(message.firstMessage);
+console.log('After 3 seconds you will see the delayed message.');
+message.delayedMessage();
+
+const img = makeImage(platziImage);
+render(img);
